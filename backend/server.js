98c7b26aae5373fe6a95b6c0d30388d2cf9777da
Nikhil Sam Mani <nikhilsm1996@gmail.com>
2025-05-16 10:47:38 +0530

    mongo uri

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const connectDB = require('./src/config/db');
 const todoRoutes = require('./src/routes/todoRoutes');
+require('dotenv').config();
 
 // Initialize express app
 const app = express();
